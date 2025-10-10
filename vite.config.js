953415768d424b1776b3601691b3b43e06b9ab86
Alexis Jose Valera Valbuena <66097641+Alexiva1995@users.noreply.github.com>
2025-10-10 10:21:47 -0400

    Revert "Migrate from Laravel Mix to Vite"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/assets/scss/style-rtl.scss',
+                'resources/assets/scss/style.scss',
+                'resources/sass/base/custom-rtl.scss',
+                'resources/sass/core.scss',
+                'resources/sass/overrides.scss',
+                'resources/assets/js/scripts.js',
+                'resources/js/core/app-menu.js',
+                'resources/js/core/app.js',
+            ],
             refresh: true,
         }),
     ],
